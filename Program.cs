094dec0f69aa76cafd60af03fc74957f606827e5
Karina Gwalani <128471243+Karina-Abjima@users.noreply.github.com>
2023-03-21 18:40:59 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿class MyClass
+{
+    
+    string color = "red";        
+    int maxSpeed = 200;          
+    public void fullThrottle()  
+    {
+        Console.WriteLine("The car is going as fast as it can!");
+    }
+    static void Main(string []args)
+    {
+        MyClass obj = new MyClass();
+        obj.color = "red";
+        Console.WriteLine(obj.color);
+        obj.fullThrottle();
+    }
+}
